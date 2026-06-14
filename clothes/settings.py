2b955c07c549ec12341d602aa018e12297b9337d
Gurtejbir Singh <gtbsbrilliant123@gmail.com>
2026-06-14 14:00:46 +0530

    fix: resolve invalid hosts

--- clothes/settings.py
+++ clothes/settings.py
@@ -132,3 +132,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Tell Django to trust the standard cloud reverse-proxy headers
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
