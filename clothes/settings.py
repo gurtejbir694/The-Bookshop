58324acdfb982813dda0bad7db137f41ab440f7a
Gurtejbir Singh <gtbsbrilliant123@gmail.com>
2026-06-14 13:17:53 +0530

    chore: configure multi-environment settings and gunicorn dependencies

--- clothes/settings.py
+++ clothes/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from pathlib import BASE_DIR
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rnsda+-1a_5=q7@!2q+!4mdz-^rru0fv@x)948d6#_+ly_@_cz'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-rnsda+-1a_5=q7@!2q+!4mdz-^rru0fv@x)948d6#_+ly_@_cz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1' '.render.com']
 
 
 # Application definition
@@ -75,12 +77,18 @@ WSGI_APPLICATION = 'clothes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
